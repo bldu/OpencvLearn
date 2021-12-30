a22b3b9c21857d4e7ff60f7c316440227f784db2
Bolun Du <duboluntc@outlook.com>
2021-12-30 19:34:09 +0800

    ...

---
+++ learn_examples/mouse_line.cpp
@@ -0,0 +1,46 @@
+// https://blog.csdn.net/qq_29540745/article/details/52562101
+
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <string>
+
+#define WINDOW "11"
+static bool P = false;
+static cv::Point previousPoint;
+static cv::Mat img;
+
+static void UseMouse1(int event, int x, int y, int flags, void* param);
+
+int main(int argc, char* argv[]) {
+  std::string img_path =
+      "/Users/dbl/NewEra/NoobPractice/OpencvLearn/images/lenna.jpeg";
+  img = cv::imread(img_path);
+  cv::imshow(WINDOW, img);
+  cv::setMouseCallback(WINDOW, UseMouse1, 0);
+  cv::waitKey(0);
+  return 0;
+}
+
+void UseMouse1(int event, int x, int y, int flags, void* param) {
+  switch (event) {
+    case cv::EVENT_LBUTTONUP: {
+      P = false;
+    } break;
+    case cv::EVENT_LBUTTONDOWN: {
+      previousPoint = cv::Point(x, y);
+
+      P = true;
+    } break;
+
+    case cv::EVENT_MOUSEMOVE: {
+      if (P) {
+        cv::Point pt(x, y);
+        cv::line(img, previousPoint, pt, cv::Scalar(0, 0, 255), 2, 5, 0);
+        previousPoint = pt;
+        cv::imshow(WINDOW, img);
+      }
+    } break;
+  }
+}
+
+void UseMouse2(){};
---
+++ learn_examples/simple_animation.cpp
@@ -0,0 +1,60 @@
+// https://blog.csdn.net/cyhbrilliant/article/details/50471694
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <string>
+
+void MakeAnimation1() {
+  std::string img_path =
+      "/Users/dbl/NewEra/NoobPractice/OpencvLearn/images/lenna.jpeg";
+  cv::Mat img = cv::imread(img_path);
+
+  int a = 0;
+  int b = 0;
+  int t = 0;
+  int k = 1;
+
+  while (true) {
+    cv::circle(img, cv::Point(b, a), k, cv::Scalar(255, 0, 255), 3);  //画圆
+    cv::imshow("animation1", img);
+    cv::waitKey(50);  // 20ms一帧图像
+    a += 1;
+    b += 1;
+    k++;  //改变控制圆大小
+  }
+}
+
+void MakeAnimation2() {
+  std::string img_path =
+      "/Users/dbl/NewEra/NoobPractice/OpencvLearn/images/lenna.jpeg";
+  cv::Mat img = cv::imread(img_path);
+  cv::Mat img_ani(img.rows, img.cols, CV_8UC3);
+
+  int a = 0;
+  int b = 0;
+  int t = 0;
+  int k = 1;
+
+  while (true) {
+    img_ani = img.clone();
+    cv::circle(img_ani, cv::Point(b, a), k, cv::Scalar(255, 0, 255), 3);  //画圆
+    cv::imshow("animation2", img_ani);
+    cv::waitKey(50);  // 20ms一帧图像
+    a += 1;
+    b += 1;
+    k++;  //改变控制圆大小
+  }
+}
+
+void MakeAnimation3() {
+  std::string img_path =
+      "/Users/dbl/NewEra/NoobPractice/OpencvLearn/images/lenna.jpeg";
+  cv::Mat img = cv::imread(img_path);
+}
+
+int main(int argc, char *argv[]) {
+  //  MakeAnimation1();
+  MakeAnimation2();
+  return 0;
+}
